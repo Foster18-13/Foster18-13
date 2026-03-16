65a63342e23dfa7f42d6021799b16e1b8370b092
ANTWI FOSTER FRIMPONG <ffantwi@st.ug.edu.gh>
2026-03-16 01:55:12 +0000

    Hide sidebar navigation by default

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,3 @@
-document.documentElement.classList.add("js-nav");
-
 function markActiveNav() {
   const current = location.pathname.split("/").pop() || "home.html";
   document.querySelectorAll(".main-nav a").forEach((link) => {
